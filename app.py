2895ad11b5a642b590a84c6032bc654d542414a8
Your Name <you@example.com>
2025-07-25 22:43:47 +0700

    Final order

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ worksheet = get_worksheet()
 df = load_data(worksheet)
 
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Add Entry", "Edit Entry", "Delete Entry", "View Calendar and AI Ideas", "Analytics", ])
+page = st.sidebar.radio("Go to", ["Home", "Add Entry", "Edit Entry", "Delete Entry", "Calendar & Ideas", "Analytics", ])
 
 if page == "Home":
     import pages.home as home
@@ -17,7 +17,7 @@ elif page == "Add Entry":
     st.title("Add Entry")
     add_entry.show(worksheet)
 
-elif page == "View Calendar and AI Ideas":
+elif page == "Calendar & Ideas":
     import pages.view_calendar as view_calendar
     st.title("📋 Current Calendar with AI Ideas")
     view_calendar.show(df)
