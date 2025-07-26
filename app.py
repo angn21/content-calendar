1d784752e56ea480069c27dd57323b48b0799825
Your Name <you@example.com>
2025-07-26 19:19:58 +0700

    changing page folder name

--- app.py
+++ app.py
@@ -8,32 +8,32 @@ st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Home", "Add Entry", "Edit Entry", "Delete Entry", "Calendar & Ideas", "Analytics", ])
 
 if page == "Home":
-    import pages.home as home
+    import _pages.home as home
     st.title("Home")
     home.show()
 
 elif page == "Add Entry":
-    import pages.add_entry as add_entry
+    import _pages.add_entry as add_entry
     st.title("Add Entry")
     add_entry.show(worksheet)
 
 elif page == "Calendar & Ideas":
-    import pages.view_calendar as view_calendar
+    import _pages.view_calendar as view_calendar
     st.title("📋 Current Calendar with AI Ideas")
     view_calendar.show(df)
 
 elif page == "Analytics":
-    import pages.analytics as analytics
+    import _pages.analytics as analytics
     st.title("📊 Analytics Dashboard")
     analytics.show(df)
 
 elif page == "Edit Entry":
-    import pages.edit_entry as edit_entry
+    import _pages.edit_entry as edit_entry
     st.title("✏️ Edit Existing Post")
     edit_entry.show(df, worksheet)
 
 elif page == "Delete Entry":
-    import pages.delete_entry as delete_entry
+    import _pages.delete_entry as delete_entry
     st.title("🗑️ Delete a Post")
     delete_entry.show(df, worksheet)
 
