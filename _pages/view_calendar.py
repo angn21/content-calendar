9bea63e699e53205c47176a74e5676f02936183d
Your Name <you@example.com>
2025-07-27 11:30:23 +0700

    testing table changes

--- _pages/view_calendar.py
+++ _pages/view_calendar.py
@@ -81,7 +81,7 @@ def show(df):
 
     # Render as markdown table
     st.markdown("### 📅 Your Posts")
-    st.markdown(filtered_display_df.to_markdown(index=False), unsafe_allow_html=True)
+    st.dataframe(filtered_display_df, use_container_width=True)
 
 
     if not filtered_df.empty:
